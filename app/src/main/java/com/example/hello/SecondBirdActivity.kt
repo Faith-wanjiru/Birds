4a03b42836cb6e892c5838df0861e0094ec7e53e
Faith-Wanjiru <158447137+Faith-wanjiru@users.noreply.github.com>
2024-06-11 15:12:12 +0300

    SecondBirdActivity.kt

--- app/src/main/java/com/example/hello/SecondBirdActivity.kt
+++ app/src/main/java/com/example/hello/SecondBirdActivity.kt
@@ -22,6 +22,7 @@ class SecondBirdActivity : AppCompatActivity() {
             Val intent = Intent(this, ThirdBirdActivity::class.java)
             startActivity(intent)
         }
-       picasso.get().load("https://images.unsplash.com/photo-1493236296276-d17357e28888?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").into(
+       picasso.get().load("https://images.unsplash.com/photo-1493236296276-d17357e28888?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+            .into(binding.)
     }
 }
